797f1319f1837c1dade6ce37ec0a72fd3cf7b0a9
PunkCactus <78229011+PunkCactus@users.noreply.github.com>
2021-12-16 12:42:05 -0300

    Corrige código e integra el script

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var contar= 1;
+var contador= 0;
 document.getElementById('btn-añadir').onclick= function () {AñadirElemento()};
 function AñadirElemento(){
-    var ContenidoLista= document.getElementById('Lista').value;
-    if(ContenidoLista!="" && ContenidoLista!=" "){
+    var ContenidoLista= document.getElementById('Input').value;
+    if(ContenidoLista!=length && ContenidoLista!=" "){
         //Crear lista
         var nuevoelemento= document.createElement('li')
         nuevoelemento.className= 'item-lista';
         nuevoelemento.id='item-lista';
         var ListaCompleta= document.getElementById('ItemsLista');
+        var nuevotexto= document.createTextNode(ContenidoLista);
+        nuevoelemento.appendChild(nuevotexto);
         ListaCompleta.appendChild(nuevoelemento);
+        Input.value='';
+        contador++;
     }
 }
+
